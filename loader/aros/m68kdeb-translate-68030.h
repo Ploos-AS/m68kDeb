a04c86abc5ba6ef5af27bab2e5160b6b14f4f4c6
pos <pgousdal@users.noreply.github.com>
2026-09-10 12:37:56 +0200

    M1.3b.4b.5h expose 16-bit PMMU status

--- loader/aros/m68kdeb-translate-68030.h
+++ loader/aros/m68kdeb-translate-68030.h
@@ -2,7 +2,7 @@
 #define M68KDEB_TRANSLATE_68030_H
 
 int m68kdeb_translate_68030_ptest(unsigned long logical,
-                                  unsigned long *mmusr_out,
+                                  unsigned short *psr_out,
                                   unsigned long *last_descriptor_phys_out);
 
 #endif
