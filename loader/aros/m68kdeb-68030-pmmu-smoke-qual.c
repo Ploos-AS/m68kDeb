7a1d08bd9f56590e296647b621c85156969661cd
pos <pgousdal@users.noreply.github.com>
2026-09-12 23:47:31 +0200

    Restore exact known-good PMMU smoke qualifier

--- loader/aros/m68kdeb-68030-pmmu-smoke-qual.c
+++ loader/aros/m68kdeb-68030-pmmu-smoke-qual.c
@@ -17,7 +17,6 @@
 #define PAGE_INDEX_SHIFT 12UL
 #define TABLE_DESC 0x0000000aUL
 #define PAGE_DESC 0x00000019UL
-#define LOGICAL_ALIAS_PAGE 0x005db000UL
 
 extern unsigned char m68kdeb_pmmu_smoke_blob[];
 extern unsigned int m68kdeb_pmmu_smoke_blob_len;
@@ -39,102 +38,74 @@ static LONG marker(const char *path, const char *text)
     return n > 0 ? 0 : 20;
 }
 
-static LONG run_stage(smoke_fn_t fn, ULONG *srp)
-{
-    APTR old_super;
-    LONG rc;
-    Disable();
-    old_super = SuperState();
-    rc = fn(srp);
-    if (old_super) UserState(old_super);
-    Enable();
-    return rc;
-}
-
 int main(void)
 {
-    UBYTE *table_raw = NULL, *tramp_raw = NULL, *dummy_raw = NULL;
-    ULONG table_page, tramp_page, dummy_page;
-    ULONG *root, *ptr_phys, *ptr_log, *pte_phys, *pte_log;
+    UBYTE *table_raw = NULL, *tramp_raw = NULL;
+    ULONG table_page, tramp_page;
+    ULONG *root, *ptr, *pte;
     ULONG srp[2];
-    ULONG pri, ppi, pti, lri, lpi, lti;
-    smoke_fn_t fn;
+    ULONG ri, pi, ti;
+    APTR old_super;
     LONG rc = 20;
 
     Printf("M68KDEB_PMMU_SMOKE_START\n");
-    if (!m68kdeb_pmmu_smoke_blob_len || m68kdeb_pmmu_smoke_blob_len > PAGE_SIZE)
+
+    if (!m68kdeb_pmmu_smoke_blob_len || m68kdeb_pmmu_smoke_blob_len > PAGE_SIZE) {
+        Printf("FAIL blob_len=%lu\n", (ULONG)m68kdeb_pmmu_smoke_blob_len);
         return 20;
+    }
 
     table_raw = (UBYTE *)AllocMem(TABLE_RAW_BYTES, MEMF_PUBLIC | MEMF_CLEAR);
     tramp_raw = (UBYTE *)AllocMem(TRAMP_RAW_BYTES, MEMF_PUBLIC | MEMF_CLEAR);
-    dummy_raw = (UBYTE *)AllocMem(TRAMP_RAW_BYTES, MEMF_PUBLIC | MEMF_CLEAR);
-    if (!table_raw || !tramp_raw || !dummy_raw) goto out;
+    if (!table_raw || !tramp_raw) {
+        Printf("FAIL alloc\n");
+        goto out;
+    }
 
     table_page = align_page((ULONG)table_raw);
     tramp_page = align_page((ULONG)tramp_raw);
-    dummy_page = align_page((ULONG)dummy_raw);
-
-    /* Keep the original known-PASS physical chain at exactly +0/+512/+1024. */
     root = (ULONG *)table_page;
-    ptr_phys = (ULONG *)(table_page + 512UL);
-    pte_phys = (ULONG *)(table_page + 1024UL);
-    ptr_log = (ULONG *)(table_page + 1280UL);
-    pte_log = (ULONG *)(table_page + 1792UL);
+    ptr = (ULONG *)(table_page + 512UL);
+    pte = (ULONG *)(table_page + 1024UL);
 
     CopyMem(m68kdeb_pmmu_smoke_blob, (APTR)tramp_page,
             (ULONG)m68kdeb_pmmu_smoke_blob_len);
     CacheClearU();
 
-    pri = (tramp_page >> ROOT_INDEX_SHIFT) & (ROOT_TABLE_SIZE - 1UL);
-    ppi = (tramp_page >> PTR_INDEX_SHIFT) & (PTR_TABLE_SIZE - 1UL);
-    pti = (tramp_page >> PAGE_INDEX_SHIFT) & (PAGE_TABLE_SIZE - 1UL);
-    lri = (LOGICAL_ALIAS_PAGE >> ROOT_INDEX_SHIFT) & (ROOT_TABLE_SIZE - 1UL);
-    lpi = (LOGICAL_ALIAS_PAGE >> PTR_INDEX_SHIFT) & (PTR_TABLE_SIZE - 1UL);
-    lti = (LOGICAL_ALIAS_PAGE >> PAGE_INDEX_SHIFT) & (PAGE_TABLE_SIZE - 1UL);
+    ri = (tramp_page >> ROOT_INDEX_SHIFT) & (ROOT_TABLE_SIZE - 1UL);
+    pi = (tramp_page >> PTR_INDEX_SHIFT) & (PTR_TABLE_SIZE - 1UL);
+    ti = (tramp_page >> PAGE_INDEX_SHIFT) & (PAGE_TABLE_SIZE - 1UL);
 
-    root[pri] = ((ULONG)ptr_phys & 0xffffff00UL) | TABLE_DESC;
-    ptr_phys[ppi] = ((ULONG)pte_phys & 0xffffff00UL) | TABLE_DESC;
-    pte_phys[pti] = (tramp_page & 0xfffff000UL) | PAGE_DESC;
+    root[ri] = ((ULONG)ptr & 0xffffff00UL) | TABLE_DESC;
+    ptr[pi] = ((ULONG)pte & 0xffffff00UL) | TABLE_DESC;
+    pte[ti] = (tramp_page & 0xfffff000UL) | PAGE_DESC;
 
     srp[0] = 0x80000002UL;
     srp[1] = (ULONG)root;
-    fn = (smoke_fn_t)tramp_page;
 
-    Printf("table=%08lx tramp=%08lx dummy=%08lx pri=%lu ppi=%lu pti=%lu lri=%lu lpi=%lu lti=%lu\n",
-           table_page, tramp_page, dummy_page, pri, ppi, pti, lri, lpi, lti);
-    marker("SYS:m1-3b4b6b-pmmu-armed.marker", "exact-trampoline descriptor bisect armed\n");
+    Printf("table=0x%08lx tramp=0x%08lx ri=%lu pi=%lu ti=%lu\n",
+           table_page, tramp_page, ri, pi, ti);
+    Printf("srp=%08lx:%08lx root=%08lx ptr=%08lx pte=%08lx\n",
+           srp[0], srp[1], root[ri], ptr[pi], pte[ti]);
 
-    /* Stage 0 must reproduce the original known-PASS experiment. */
-    rc = run_stage(fn, srp);
-    if (rc != 0) goto out;
-    marker("SYS:m1-3b4b6b-pmmu-identity-pass.marker", "identity-only passed\n");
+    marker("SYS:m1-3b4b6b-pmmu-armed.marker", "PMMU smoke armed\n");
 
-    /* Add one descriptor level at a time, never executing through this alias. */
-    root[lri] = ((ULONG)ptr_log & 0xffffff00UL) | TABLE_DESC;
-    CacheClearU();
-    rc = run_stage(fn, srp);
-    if (rc != 0) goto out;
-    marker("SYS:m1-3b4b6b-pmmu-root-pass.marker", "second root passed\n");
+    Disable();
+    old_super = SuperState();
+    rc = ((smoke_fn_t)tramp_page)(srp);
+    if (old_super) UserState(old_super);
+    Enable();
 
-    ptr_log[lpi] = ((ULONG)pte_log & 0xffffff00UL) | TABLE_DESC;
-    CacheClearU();
-    rc = run_stage(fn, srp);
-    if (rc != 0) goto out;
-    marker("SYS:m1-3b4b6b-pmmu-pointer-pass.marker", "second pointer passed\n");
-
-    pte_log[lti] = (dummy_page & 0xfffff000UL) | PAGE_DESC;
-    CacheClearU();
-    rc = run_stage(fn, srp);
-    if (rc != 0) goto out;
-    marker("SYS:m1-3b4b6b-pmmu-fullchain-pass.marker", "full second chain passed\n");
-    marker("SYS:m1-3b4b6b-pmmu-prealias-pass.marker", "descriptor bisect passed\n");
-    marker("SYS:m1-3b4b6b-pmmu-returned.marker", "descriptor bisect returned\n");
-    marker("SYS:m1-3b4b6b-pmmu-pass.marker", "PMMU smoke passed\n");
-    Printf("M68KDEB_PMMU_PREALIAS_PASS\nM68KDEB_PMMU_SMOKE_PASS\n");
-    rc = 0;
+    Printf("M68KDEB_PMMU_SMOKE_RETURN rc=%ld\n", rc);
+    if (rc == 0) {
+        marker("SYS:m1-3b4b6b-pmmu-returned.marker", "PMMU smoke returned\n");
+        marker("SYS:m1-3b4b6b-pmmu-pass.marker", "PMMU smoke passed\n");
+        Printf("M68KDEB_PMMU_SMOKE_PASS\n");
+    } else {
+        marker("SYS:m1-3b4b6b-pmmu-fail.marker", "PMMU smoke returned failure\n");
+    }
 
 out:
-    if (dummy_raw) FreeMem(dummy_raw, TRAMP_RAW_BYTES);
     if (tramp_raw) FreeMem(tramp_raw, TRAMP_RAW_BYTES);
     if (table_raw) FreeMem(table_raw, TABLE_RAW_BYTES);
     return rc == 0 ? 0 : 20;
