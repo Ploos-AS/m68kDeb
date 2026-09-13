7e1fb5f7e733428bfbcf1ed44908f95e2e54de30
pos <pgousdal@users.noreply.github.com>
2026-09-14 01:12:02 +0200

    M1.3b.4b.6b identity-map 16MiB transition window

--- loader/aros/m68kdeb-68030-pmmu-smoke-qual.c
+++ loader/aros/m68kdeb-68030-pmmu-smoke-qual.c
@@ -7,7 +7,7 @@
 #include <string.h>
 
 #define PAGE_SIZE 4096UL
-#define TABLE_RAW_BYTES (PAGE_SIZE * 2UL - 1UL)
+#define TABLE_RAW_BYTES (32768UL + PAGE_SIZE - 1UL)
 #define TRAMP_RAW_BYTES (PAGE_SIZE * 2UL - 1UL)
 #define ROOT_TABLE_SIZE 128UL
 #define PTR_TABLE_SIZE 128UL
@@ -17,7 +17,9 @@
 #define PAGE_INDEX_SHIFT 12UL
 #define TABLE_DESC 0x0000000aUL
 #define PAGE_DESC 0x00000019UL
-#define LOGICAL_ALIAS_PAGE 0x005db000UL
+#define IDENTITY_LIMIT 0x01000000UL
+#define IDENTITY_PTRS (IDENTITY_LIMIT >> PTR_INDEX_SHIFT)
+#define LOGICAL_ALIAS_PAGE 0x01000000UL
 
 extern unsigned char m68kdeb_pmmu_smoke_blob[];
 extern unsigned int m68kdeb_pmmu_smoke_blob_len;
@@ -55,10 +57,11 @@ static LONG run_phase(smoke_fn_t smoke, ULONG *srp, ULONG phase)
 int main(void)
 {
     UBYTE *table_raw = NULL, *tramp_raw = NULL;
-    ULONG table_page, tramp_page;
-    ULONG *root, *ptr, *pte, *ptr_log, *pte_log;
+    ULONG table_page, tramp_page, pte_base;
+    ULONG *root, *ptr, *pte, *alias_pte;
     ULONG srp[2];
     ULONG ri, pi, ti, lri, lpi, lti;
+    ULONG pidx, tidx, phys;
     smoke_fn_t smoke;
     LONG rc = 20;
 
@@ -80,39 +83,54 @@ int main(void)
     tramp_page = align_page((ULONG)tramp_raw);
     root = (ULONG *)table_page;
     ptr = (ULONG *)(table_page + 512UL);
-    pte = (ULONG *)(table_page + 1024UL);
-    ptr_log = (ULONG *)(table_page + 1280UL);
-    pte_log = (ULONG *)(table_page + 1792UL);
+    pte_base = table_page + 1024UL;
+
+    if (tramp_page >= IDENTITY_LIMIT) {
+        Printf("FAIL trampoline outside identity window: 0x%08lx\n", tramp_page);
+        goto out;
+    }
 
     CopyMem(m68kdeb_pmmu_smoke_blob, (APTR)tramp_page,
             (ULONG)m68kdeb_pmmu_smoke_blob_len);
     CacheClearU();
     smoke = (smoke_fn_t)tramp_page;
 
-    ri = (tramp_page >> ROOT_INDEX_SHIFT) & (ROOT_TABLE_SIZE - 1UL);
-    pi = (tramp_page >> PTR_INDEX_SHIFT) & (PTR_TABLE_SIZE - 1UL);
-    ti = (tramp_page >> PAGE_INDEX_SHIFT) & (PAGE_TABLE_SIZE - 1UL);
+    /* Build a complete 0-16 MiB identity map. A one-page identity map is too
+     * narrow for a safe PMMU transition because the supervisor stack and any
+     * prefetched/runtime accesses must remain valid while TC is enabled. */
+    root[0] = ((ULONG)ptr & 0xfffffff0UL) | TABLE_DESC;
+    for (pidx = 0; pidx < IDENTITY_PTRS; ++pidx) {
+        pte = (ULONG *)(pte_base + pidx * (PAGE_TABLE_SIZE * sizeof(ULONG)));
+        ptr[pidx] = ((ULONG)pte & 0xfffffff0UL) | TABLE_DESC;
+        for (tidx = 0; tidx < PAGE_TABLE_SIZE; ++tidx) {
+            phys = (pidx << PTR_INDEX_SHIFT) | (tidx << PAGE_INDEX_SHIFT);
+            pte[tidx] = (phys & 0xfffff000UL) | PAGE_DESC;
+        }
+    }
+
+    /* The alias lives immediately above the identity window so it exercises
+     * a real translated fetch without stealing an address that AROS may use. */
     lri = (LOGICAL_ALIAS_PAGE >> ROOT_INDEX_SHIFT) & (ROOT_TABLE_SIZE - 1UL);
     lpi = (LOGICAL_ALIAS_PAGE >> PTR_INDEX_SHIFT) & (PTR_TABLE_SIZE - 1UL);
     lti = (LOGICAL_ALIAS_PAGE >> PAGE_INDEX_SHIFT) & (PAGE_TABLE_SIZE - 1UL);
+    alias_pte = (ULONG *)(pte_base + IDENTITY_PTRS * (PAGE_TABLE_SIZE * sizeof(ULONG)));
+    ptr[lpi] = ((ULONG)alias_pte & 0xfffffff0UL) | TABLE_DESC;
+    alias_pte[lti] = (tramp_page & 0xfffff000UL) | PAGE_DESC;
 
-    root[ri] = ((ULONG)ptr & 0xffffff00UL) | TABLE_DESC;
-    ptr[pi] = ((ULONG)pte & 0xffffff00UL) | TABLE_DESC;
-    pte[ti] = (tramp_page & 0xfffff000UL) | PAGE_DESC;
-
-    root[lri] = ((ULONG)ptr_log & 0xffffff00UL) | TABLE_DESC;
-    ptr_log[lpi] = ((ULONG)pte_log & 0xffffff00UL) | TABLE_DESC;
-    pte_log[lti] = (tramp_page & 0xfffff000UL) | PAGE_DESC;
+    ri = (tramp_page >> ROOT_INDEX_SHIFT) & (ROOT_TABLE_SIZE - 1UL);
+    pi = (tramp_page >> PTR_INDEX_SHIFT) & (PTR_TABLE_SIZE - 1UL);
+    ti = (tramp_page >> PAGE_INDEX_SHIFT) & (PAGE_TABLE_SIZE - 1UL);
+    pte = (ULONG *)(pte_base + pi * (PAGE_TABLE_SIZE * sizeof(ULONG)));
 
     srp[0] = 0x80000002UL;
     srp[1] = (ULONG)root;
 
-    Printf("table=0x%08lx tramp=0x%08lx ri=%lu pi=%lu ti=%lu\n",
-           table_page, tramp_page, ri, pi, ti);
-    Printf("srp=%08lx:%08lx root=%08lx ptr=%08lx pte=%08lx\n",
-           srp[0], srp[1], root[ri], ptr[pi], pte[ti]);
-    Printf("alias lri=%lu lpi=%lu lti=%lu root=%08lx ptr=%08lx pte=%08lx\n",
-           lri, lpi, lti, root[lri], ptr_log[lpi], pte_log[lti]);
+    Printf("table=0x%08lx tramp=0x%08lx identity_limit=0x%08lx\n",
+           table_page, tramp_page, (ULONG)IDENTITY_LIMIT);
+    Printf("identity ri=%lu pi=%lu ti=%lu root=%08lx ptr=%08lx pte=%08lx\n",
+           ri, pi, ti, root[ri], ptr[pi], pte[ti]);
+    Printf("alias=0x%08lx lri=%lu lpi=%lu lti=%lu ptr=%08lx pte=%08lx\n",
+           (ULONG)LOGICAL_ALIAS_PAGE, lri, lpi, lti, ptr[lpi], alias_pte[lti]);
 
     marker("SYS:m1-3b4b6b-pmmu-armed.marker", "PMMU phased smoke armed\n");
 
